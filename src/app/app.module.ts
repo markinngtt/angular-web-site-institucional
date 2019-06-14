15dc282fd0e6c5ffd5ac2d67ae8a23a6fa90706d
Marcos Gatti <markinngtt@gmail.com>
2019-06-14 15:08:53 -0300

    Incluido modulo quem somos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuemSomosComponent
   ],
   imports: [
     BrowserModule,
